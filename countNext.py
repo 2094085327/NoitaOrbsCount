73e2027403e81f9e519e184fb1f6199e9aa3c906
2094085327 <2094085327@qq.com>
2023-07-16 11:22:45 +0800

    优化了输出部分的代码屎山，增加魔球溢出提醒，增加readme

--- countNext.py
+++ countNext.py
@@ -1,26 +1,40 @@
 import readFile
 
 
+def printMessage(message):
+    print(message)
+
+
 def getNextOrbsNum():
     ngPlusNum = int(readFile.getWorldPlusNum())
     orbsGetNow = readFile.getOrbsNum()
-    if int(ngPlusNum) == 0:
-        print("现在是初始世界")
-    else:
-        print("现在是第 " + str(ngPlusNum) + " 个新世界(NG+" + str(ngPlusNum) + ")")
 
-    print("当前周目已经获取到 " + str(orbsGetNow) + " 个魔球")
+    if ngPlusNum == 0:
+        printMessage("现在是初始世界")
+    else:
+        printMessage(f"现在是第 {ngPlusNum} 个新世界(NG+{ngPlusNum})")
+
+    printMessage(f"当前周目已经获取到 {orbsGetNow} 个魔球")
 
     if ngPlusNum < 6:
-        print("能够进入NG+" + str(ngPlusNum + 1) + "的魔球数为: " + str(5 + ngPlusNum) + "-10")
-        print("进入下一世界还需要获得至少 " + str(5 + ngPlusNum - orbsGetNow) + " 个魔球")
+        orbsNeeded = 5 + ngPlusNum - orbsGetNow
+        orbsRange = f"{5 + ngPlusNum}-10"
+        if orbsGetNow > 10:
+            printMessage(
+                f"你已经获取到的魔球数已经超出了能够进入下一周目的魔球数，可以进入下一周目的魔球数最大为:{orbsRange}")
+        else:
+            printMessage(f"进入下一世界还需要获得至少 {orbsNeeded} 个魔球")
     elif ngPlusNum == 6:
-        print("能够进入NG+" + str(ngPlusNum + 1) + "的魔球数为: 10")
-        print("进入下一世界还需要获得至少 " + str(10 - orbsGetNow) + " 个魔球")
-
+        orbsNeeded = 10 - orbsGetNow
+        printMessage("能够进入NG+7的魔球数为: 10")
+        if orbsGetNow > 10:
+            printMessage("你已经获取到的魔球数已经超出了能够进入下一周目的魔球数，可以进入下一周目的魔球数最大为:10")
+        else:
+            printMessage(f"进入下一世界还需要获得至少 {orbsNeeded} 个魔球")
     elif 6 < ngPlusNum < 28:
-        print("能够进入NG+" + str(ngPlusNum + 1) + "的魔球数为:" + str(5 + ngPlusNum) + "-32")
-        print("进入下一世界还需要获得至少 " + str(5 + ngPlusNum - orbsGetNow) + " 个魔球")
-
+        orbsNeeded = 5 + ngPlusNum - orbsGetNow
+        orbsRange = f"{5 + ngPlusNum}-32"
+        printMessage(f"能够进入NG+{ngPlusNum + 1}的魔球数为:{orbsRange}")
+        printMessage(f"进入下一世界还需要获得至少 {orbsNeeded} 个魔球")
     else:
-        print("现在已经是NG+28了，总共需要33个魔球")
+        printMessage("现在已经是NG+28了，总共需要33个魔球")
